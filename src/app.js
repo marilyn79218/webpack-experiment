c259799c963e6aa5265c12e779ca486870579af8
ray8863 <marilyn79218@gmail.com>
2018-12-02 14:29:11 +0800

    1. Upgrade packages & relative config changes. 2. Babel-loader.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+// import { prop } from 'ramda';
+// import moment from 'moment';
 require('./style.scss');
+
+// const today = moment();
+const person = {
+  name: 'ray',
+  age: 20,
+  email: 'ray.lei@gmail.com',
+};
+
+const Name = ['Mike', 'Jacky', 'Andy', 'Scars'];
+Name.forEach((obj, idx)=> console.log(`${idx} => ${obj}`));
+
+// const getName = prop('name');
+// const name = getName(person);
+// console.log('name', name);
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,43 @@
+const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: './dist',
-		filename: 'static/js/build.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.scss$/,
-				loader: ExtractTextPlugin.extract({
-					fallback: 'style-loader', // In case extract failed
-					loader: 'css!sass',
-				})
-			}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin('static/css/style.css'),
-		// Add <script> and <link> into template html
-		new HtmlWebpackPlugin({
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'static/js/build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        options: {
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
+          cacheDirectory: true,
+          presets: ['@babel/preset-env']
+        },
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader', // In case of extract failed
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('static/css/style.css'),
+    // Add <script> and <link> into template html
+    new HtmlWebpackPlugin({
       inject: true,
       template: 'public/index.html',
     }),
-	],
-	watch: true,
+  ],
+  watch: true,
 };
